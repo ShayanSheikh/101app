bf4d3d2d3cf759853013f473b19dd187d56707b2
Shayan Sheikh <najam.shayan@gmail.com>
2018-02-23 14:23:41 -0500

    Infinite scroll now works

--- components/AllPictures.js
+++ components/AllPictures.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text, FlatList, Image, TouchableHighlight } from 'react-native';
 import { connect } from 'react-redux';
-import { setPic, setPage } from '../store';
+import { setPic, setPage, fetchMorePictures } from '../store';
 
 class AllPictures extends React.Component {
   constructor(props) {
@@ -9,6 +9,14 @@ class AllPictures extends React.Component {
     this.state = {
       page: 1
     }
+    this.handleLoadMore = this.handleLoadMore.bind(this);
+  }
+
+  handleLoadMore() {
+    if (this.state.page >= 25) return;
+    this.setState({
+      page: this.state.page + 1
+    }, () => this.props.getPictures(this.props.query, this.state.page))
   }
 
   render() {
@@ -18,23 +26,26 @@ class AllPictures extends React.Component {
       (
         <View style={styles.container}>
           <FlatList
+            style={styles.flatlist}
             data={pictures}
             renderItem={({ item }) => {
               return (
               <TouchableHighlight onPress={() => {
-                  setPicInfo(item, this.state.page);
+                  setPicInfo(item);
                   navigation.navigate('SinglePicture');
                 }}>
                 <Image
                   style={styles.image}
                   key={item.id}
                   source={{ uri: item.previewURL }}
-                  onC
                 />
               </TouchableHighlight>
               )
             }}
             keyExtractor={item => item.id}
+            onEndThreshold={0}
+            onEndReached={this.handleLoadMore}
+            numColumns={2}
           />
         </View>
       )
@@ -50,9 +61,10 @@ class AllPictures extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-around'
+    justifyContent: 'space-between'
+  },
+  flatlist: {
+    flexDirection: 'column',
   },
   image: {
     height: 150,
@@ -62,15 +74,18 @@ const styles = StyleSheet.create({
 
 const mapState = (state) => {
   return {
-    pictures: state.pictures
+    pictures: state.pictures,
+    query: state.utils.query
   }
 }
 
 const mapDispatch = (dispatch) => {
   return {
-    setPicInfo: (pic, page) => {
+    setPicInfo: (pic) => {
       dispatch(setPic(pic));
-      dispatch(setPage(page));
+    },
+    getPictures: (query, page) => {
+      dispatch(fetchMorePictures(query, page));
     }
   }
 }
